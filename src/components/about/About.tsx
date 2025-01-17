0d83f222ace08b24be97064024dd8aa17acd42b6
Manish2889 <manishr2889@gmail.com>
2025-01-17 08:47:47 +0530

    removed team section

---
+++ src/components/about/About.tsx
@@ -0,0 +1,19 @@
+// Import the TeamSection component
+// import TeamSection from '../sections/TeamSection';
+
+const About = () => {
+  return (
+    <div>
+      {/* Other sections of the About Us page */}
+      
+      {/* Team Section */}
+      {/* 
+      <TeamSection />
+      */}
+      
+      {/* Other sections of the About Us page */}
+    </div>
+  )
+}
+
+export default About 
